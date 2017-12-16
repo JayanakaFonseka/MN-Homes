841c4bdf2fee990f404431eb2f95a088ff2ccdfe
rakhdass <rakhithadassanayake93@gmail.com>
2017-12-16 11:55:39 +0530

    Back butten

--- application/views/siteofficer/add.php
+++ application/views/siteofficer/add.php
@@ -28,8 +28,8 @@
         <!-- Sidebar Column -->
         <div class="col-lg-3 mb-4">
           <div class="list-group text-center" >
-            <a href="<?php echo base_url('SiteOfficer/Home'); ?>" class="list-group-item active">Profile</a>
-            <a href="<?php echo base_url('SiteOfficer/Markattendance'); ?>" class="list-group-item">Mark Attendence</a>
+            <a href="<?php echo base_url('SiteOfficer/Home'); ?>" class="list-group-item ">Profile</a>
+            <a href="<?php echo base_url('SiteOfficer/Markattendance'); ?>" class="list-group-item active">Mark Attendence</a>
             <a href="<?php echo base_url('SiteOfficer/projectprogress'); ?>" class="list-group-item">Project progress</a>
             <a href="<?php echo base_url('SiteOfficer/inventryrequest'); ?>" class="list-group-item">Inventry Requests</a>
             <a href="<?php echo base_url('SiteOfficer/monthlyprojectreport'); ?>" class="list-group-item">Monthly Project Report</a>
@@ -47,7 +47,7 @@
           <div class="panelheading">
             <h2>            
               <a href="view.php" class="btn btn-success pull-right">view all</a>
-              <a href="add.php" class="btn btn-primary ">back</a>
+              <a href="<?php echo base_url('SiteOfficer/Markattendance'); ?>" class="btn btn-primary ">back</a>
             </h2>
 
           </div>
@@ -79,7 +79,7 @@
               <div class="form-group">
                 <label for="name">Gender:</label><br>
               
-                <input style="text-align: center" type="radio" name="gender" value="male"> Male<br>
+                <input  type="radio" name="gender" value="male"> Male<br>
                 <input type="radio" name="gender" value="female"> Female<br>
               </div>
               
