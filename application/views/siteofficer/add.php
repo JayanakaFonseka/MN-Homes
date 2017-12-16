d43e9be8272fc20925e24128675695d5d0715a75
rakhdass <rakhithadassanayake93@gmail.com>
2017-12-16 11:17:45 +0530

    mark attendance add laour butten

--- application/views/siteofficer/add.php
+++ application/views/siteofficer/add.php
@@ -1,4 +1,4 @@
-    <?php if ($this->session->userdata('loggedin')) {
+   <?php if ($this->session->userdata('loggedin')) {
       include 'loggedin/header.php';
     }
     else{
@@ -28,15 +28,15 @@
         <!-- Sidebar Column -->
         <div class="col-lg-3 mb-4">
           <div class="list-group text-center" >
-            <a href="<?php echo base_url('SiteOfficer/Home'); ?>" class="list-group-item ">Profile</a>
-            <a href="<?php echo base_url('SiteOfficer/Markattendance'); ?>" class="list-group-item active">Mark Attendence</a>
+            <a href="<?php echo base_url('SiteOfficer/Home'); ?>" class="list-group-item active">Profile</a>
+            <a href="<?php echo base_url('SiteOfficer/Markattendance'); ?>" class="list-group-item">Mark Attendence</a>
             <a href="<?php echo base_url('SiteOfficer/projectprogress'); ?>" class="list-group-item">Project progress</a>
             <a href="<?php echo base_url('SiteOfficer/inventryrequest'); ?>" class="list-group-item">Inventry Requests</a>
             <a href="<?php echo base_url('SiteOfficer/monthlyprojectreport'); ?>" class="list-group-item">Monthly Project Report</a>
 
           </div>
         </div>
-        <!-- Content Column -->
+  <!-- Content Column -->
       <div class="col-lg-9 mb-4">
         <div class="welltextcenter">
           <h2>
@@ -56,13 +56,11 @@
               <strong>DATE: <?php $cur_date=date('y-m-d'); echo $cur_date; ?></strong>
             </div> 
             <form action="" method="post">
-              <table class="table table-striped">
-                <tr>
-                <th width="25%">Labour Id</th>
-                <th width="25%">Labour Name</th>
-                <th width="25%">Attendance</th>
-                </tr>
-              </table>
+              <div class="form-group">
+                <label for="name">Labour Name</label>
+                <input type="text" id="labourname" name="labourname" required="">
+              </div>
+              
             </form>
           </div>
         </div>
