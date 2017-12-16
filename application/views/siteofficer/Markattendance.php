1b4248b2bbb71d7a6b93cf434375c9a51f53cb93
rakhdass <rakhithadassanayake93@gmail.com>
2017-12-16 09:58:25 +0530

    mark attendance view basic

--- application/views/siteofficer/Markattendance.php
+++ application/views/siteofficer/Markattendance.php
@@ -28,8 +28,8 @@
         <!-- Sidebar Column -->
         <div class="col-lg-3 mb-4">
           <div class="list-group text-center" >
-            <a href="<?php echo base_url('SiteOfficer/Home'); ?>" class="list-group-item active">Profile</a>
-            <a href="<?php echo base_url('SiteOfficer/Markattendance'); ?>" class="list-group-item">Mark Attendence</a>
+            <a href="<?php echo base_url('SiteOfficer/Home'); ?>" class="list-group-item ">Profile</a>
+            <a href="<?php echo base_url('SiteOfficer/Markattendance'); ?>" class="list-group-item active">Mark Attendence</a>
             <a href="<?php echo base_url('SiteOfficer/projectprogress'); ?>" class="list-group-item">Project progress</a>
             <a href="<?php echo base_url('SiteOfficer/inventryrequest'); ?>" class="list-group-item">Inventry Requests</a>
             <a href="<?php echo base_url('SiteOfficer/monthlyprojectreport'); ?>" class="list-group-item">Monthly Project Report</a>
@@ -37,16 +37,30 @@
           </div>
         </div>
         <!-- Content Column -->
-        <div class="col-lg-9 mb-4">
-          
+      <div class="col-lg-9 mb-4">
+        <div class="welltextcenter">
+          <h2>
+            Attendance Management
+          </h2>
+        </div>
+        <div class="panel panel default">
+          <div class="panelheading">
+            <h2>            
+              <a href="view.php" class="btn btn-success pull-right">view all</a>
+            </h2>
 
-
+          </div>
+          <div class="panel-body">
+            <div class="well text-center" style="font-size: 20px">
+              <strong>DATE: <?php $cur_date=date('y-m-d'); echo $cur_date; ?></strong>
+            </div> 
+          </div>
+        </div>
       </div>
       <!-- /.row -->
-</div>
-</div>
-
     </div>
-    <!-- /.container -->
+  </div>
+
+</div> <!-- /.container -->
 
 <?php include 'loggedin/footer.php'; ?>
