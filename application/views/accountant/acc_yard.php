4576f77a13db986e693d343a468cd5e6c357035e
Unknown <mohan.at145@gmail.com>
2017-12-17 11:47:34 +0530

    Yard added

--- application/views/accountant/acc_yard.php
+++ application/views/accountant/acc_yard.php
@@ -35,11 +35,57 @@ else{
 
             </div>
         </div>
+            <div class="col-lg-9 mb-4">
 
-        <div class="col-lg-9 mb-4">
+                <h1 >Stored Items in Yard</h1>
 
+                <hr>
+                <?php #echo form_open('Manager/searchUsers',['class'=>'form-inline']); ?>
+                <div class="form-group ">
+                    <input type="text" class="form-control col-lg-8 " id="exampleInputEmail1" placeholder="Enter any keyword.." name="search">&nbsp;&nbsp;&nbsp;&nbsp;
+                    <button type="submit" class="btn btn-default">Search</button>
+                </div>
+                <?php echo form_close(); ?>
+                <br>
+
+                <div class="table-responsive table-striped">
+                    <table class="table">
+                        <tr>
+                            <th class="text-center">Item ID</th>
+                            <th>&nbsp;&nbsp;&nbsp;&nbsp;Item Name</th>
+                            <th>&nbsp;&nbsp;&nbsp;&nbsp;Qunatity</th>
+                            <th>Unit Price</th>
+                            <th>&nbsp;&nbsp;&nbsp;&nbsp;Measuring unit</th>
+                            <th>Item Description</th>
+                        </tr>
+
+                        <?php if (count($records)): ?>
+
+                            <?php foreach ($records as $row): ?>
+
+                                <tr>
+                                    <td class="text-center"><?php echo $row->item_id; ?></td>
+                                    <td><?php echo $row->item_name ?></td>
+                                    <td><?php echo $row->item_quantity; ?></td>
+                                    <td><?php echo $row->unit_price; ?></td>
+                                    <td><?php echo $row->measure_unit ; ?></td>
+                                    <td><?php echo $row->item_des; ?></td>
+                                    <th><a href="<?php #echo base_url('Accountant/edit_item/'.$row->id) ?>"><button type='submit' class='btn btn-warning'>Update</button></th></a>
+                                </tr>
+
+                            <?php endforeach; ?>
+
+                        <?php else: ?>
+                            <h3 style="margin-bottom: 50px;">No Items in the database</h3>
+                        <?php endif ?>
+
+                    </table>
+                </div>
+
+            </div>
 
     </div>
 </div>
+
 
 <?php include 'loggedin/footer.php'; ?>
