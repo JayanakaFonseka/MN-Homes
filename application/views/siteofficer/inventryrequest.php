0cf234af10af8e8499e35ac47cf83535d22a000e
rakhdass <rakhithadassanayake93@gmail.com>
2017-12-17 13:21:31 +0530

    pageloading

--- application/views/siteofficer/inventryrequest.php
+++ application/views/siteofficer/inventryrequest.php
@@ -29,7 +29,7 @@
         <div class="col-lg-3 mb-4">
           <div class="list-group text-center" >
             <a href="<?php echo base_url('SiteOfficer/Home'); ?>" class="list-group-item ">Profile</a>
-            <a href="<?php echo base_url('SiteOfficer/viewattendance'); ?>" class="list-group-item">Mark Attendence</a>
+            <a href="<?php echo base_url('SiteOfficer/Markattendance'); ?>" class="list-group-item">Mark Attendence</a>
             <a href="<?php echo base_url('SiteOfficer/projectprogress'); ?>" class="list-group-item">Project progress</a>
             <a href="<?php echo base_url('SiteOfficer/inventryrequest'); ?>" class="list-group-item active">Inventry Requests</a>
             <a href="<?php echo base_url('SiteOfficer/monthlyprojectreport'); ?>" class="list-group-item">Monthly Project Report</a>
