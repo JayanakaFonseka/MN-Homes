cfbf697b63fab183d375e29fdf568bf979702dce
Unknown <mohan.at145@gmail.com>
2017-12-17 20:21:49 +0530

    Auto stash before merge of "master" and "origin/master"
    contact map done

--- application/models/Model_manager.php
+++ application/models/Model_manager.php
@@ -45,11 +45,11 @@ class Model_manager extends CI_Model
     function getProjects() {
 		
 		$this->db->select('*');
-		$this->db->from('projects');
-		$this->db->order_by('id');
-		$query = $this->db->get();
-        return $query->result(); 
-        
+        $this->db->from('projects');
+        $this->db->order_by('id');
+        $query = $this->db->get();
+        return $query->result();
+
     }
 
     public function edit($stu_id) {
